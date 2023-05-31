c795f970c06b5b803d9d4d4502f328c196ec2256
liushuxiang <276900785@qq.com>
2023-05-31 23:46:43 +0800

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Nextra Docs Template 
+# L-GPT Docs
 
-This is a template for creating documentation with [Nextra](https://nextra.site).
+This is a template for creating documentation with [L-GPT](https://gpt.ltopx.com).
 
 [**Live Demo →**](https://nextra-docs-template.vercel.app)
 
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>L-GPT</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "L-GPT Docs",
   },
-}
+};
 
-export default config
+export default config;
