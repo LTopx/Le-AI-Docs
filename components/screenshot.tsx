f3562822729e8ed2ebae2d39b43e82e713cf1f8a
Ethan Liu <liushuxiang19930729@gmail.com>
2023-06-14 23:03:34 +0800

    update

--- components/screenshot.tsx
+++ components/screenshot.tsx
@@ -1,12 +1,13 @@
 import cn from "clsx";
 import Image from "next/image";
 
-export function Screenshot({ src, alt, full }) {
+export function Screenshot({ src, alt, full, className }) {
   return (
     <div
       className={cn(
         "-mb-4 mt-6 flex justify-center overflow-hidden rounded-xl border dark:border-zinc-800",
-        full ? "bg-white" : "bg-zinc-100"
+        full ? "bg-white" : "bg-zinc-100",
+        className
       )}
     >
       <Image
